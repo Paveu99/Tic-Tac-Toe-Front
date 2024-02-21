7f5b947f54d6cf01a5312b90c5f4f74863fe4adb
Paweł Jarecki <paweljarecki10@gmail.com>
2024-02-21 17:15:46 +0100

    Ability to use types from backend added

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@types': 'C:/Users/W11/Desktop/Projects/Tic Tac Toe App/tictactoeback/types/*'
+    }
+  }
 })
