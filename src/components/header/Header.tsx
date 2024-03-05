4a0f3a3594f8abf2b58ca1114455b49b949a0d50
Paweł Jarecki <paweljarecki10@gmail.com>
2024-03-05 18:13:08 +0100

    New component created which represents inside of navbar component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,34 @@
+import "../styles/Header.scss";
+import {NavLink} from "react-router-dom";
+import el1 from '../styles/images/Home.png';
+import el2 from '../styles/images/Game.png';
+import el3 from '../styles/images/Charts.png';
+import el4 from '../styles/images/XO.png';
+
+export const Header = () => {
+    const styleOfLink = ({isActive}: {
+                             isActive: boolean
+                         }
+    ) => (
+        {
+            color: isActive ? "#ffffff" : '',
+            backgroundColor: isActive ? "#323232" : '',
+        }
+    )
+
+  return <header className='nav'>
+      <p className="title">TIC TAC TOE <img src={el4} alt="" className="big-icon"/></p>
+      <NavLink className="nav__link" style={styleOfLink} to="/">
+          <img className="small-icon" src={el1} alt=""/>
+          <p className="text">MAIN</p>
+      </NavLink>
+      <NavLink className="nav__link" style={styleOfLink} to="/info">
+          <img className="small-icon" src={el2} alt=""/>
+          <p className="text">GAME</p>
+      </NavLink>
+      <NavLink className="nav__link" style={styleOfLink} to="/details">
+          <img className="small-icon" src={el3} alt=""/>
+          <p className="text">LEADERBOARD</p>
+      </NavLink>
+      </header>
+}
