443dec6e826c29195d96af897aef4b1c9f1367c0
Paweł Jarecki <paweljarecki10@gmail.com>
2024-03-12 20:16:48 +0100

    Reset button - beta version without styling. Button allows to clear all fields

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,10 +1,11 @@
 import '../styles/Board.scss';
-import {useEffect, useState} from "react";
-import el1 from '../styles/images/X.png';
-import el2 from '../styles/images/O.png';
+import { useEffect, useState } from "react";
+import el1 from '../styles/images/O.png';
+import el2 from '../styles/images/X.png';
 
 export const Board = () => {
-    const [currentImg, setCurrentImg] = useState<string[]>(Array(9).fill(''));
+    const initialImgState = Array(9).fill('');
+    const [currentImg, setCurrentImg] = useState<string[]>(initialImgState);
     const [clickedIndexes, setClickedIndexes] = useState<number[]>([]);
     const [isEl1Turn, setIsEl1Turn] = useState<boolean>(true);
 
@@ -16,6 +17,12 @@ export const Board = () => {
             setClickedIndexes([...clickedIndexes, index]);
             setIsEl1Turn(!isEl1Turn);
         }
+    };
+
+    const handleReset = () => {
+        setCurrentImg(initialImgState);
+        setClickedIndexes([]);
+        setIsEl1Turn(true);
     };
 
     const clickedStyle = {
@@ -31,15 +38,18 @@ export const Board = () => {
     }, [currentImg]);
 
     return (
-        <div className="background">
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((index) => (
-                <div
-                    key={index}
-                    className='single_element'
-                    onClick={() => handleClick(index)}
-                    style={{ ...clickedStyle, backgroundImage: `url(${currentImg[index - 1]})` }}
-                ></div>
-            ))}
+        <div>
+            <div className="background">
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((index) => (
+                    <div
+                        key={index}
+                        className='single_element'
+                        onClick={() => handleClick(index)}
+                        style={{ ...clickedStyle, backgroundImage: `url(${currentImg[index - 1]})` }}
+                    ></div>
+                ))}
+            </div>
+            <button onClick={handleReset}>Reset</button>
         </div>
     );
 };
