6afabf6fe57dc821a0b57189b9b104a94528e424
Paweł Jarecki <paweljarecki10@gmail.com>
2024-03-18 17:34:34 +0100

    Component finished - styles and props handle added

--- src/components/buttons/UnderBoardBttn.tsx
+++ src/components/buttons/UnderBoardBttn.tsx
@@ -1,9 +1,11 @@
+import '../styles/UnderBoardBttn.scss'
 interface Props {
-
+    text: string;
+    onClick: () => void;
 }
 
 export const UnderBoardBttn = (props: Props) => {
-    return <button>
-
+    return <button className="underboard-button" onClick={props.onClick}>
+        {props.text}
     </button>
 }
