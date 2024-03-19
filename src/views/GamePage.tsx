0f9ac58f4e2ab88d7deff445bd3a1f1106ef7468
Paweł Jarecki <paweljarecki10@gmail.com>
2024-03-19 20:31:22 +0100

    First version of GameStartModal.tsx is used

--- src/views/GamePage.tsx
+++ src/views/GamePage.tsx
@@ -1,8 +1,28 @@
 import '../components/styles/GameView.scss'
 import {Board} from "../components/board/Board.tsx";
+import {useState} from "react";
+import {GameStartModal} from "../components/modals/GameStartModal.tsx";
+import {UnderBoardBttn} from "../components/buttons/UnderBoardBttn.tsx";
 
 export const GamePage = () => {
+
+    const [openModal, setOpenModal] = useState<boolean>(false);
+    const [inputDataCorrect, setInputDataCorrect] = useState<boolean>(false);
+
     return <div className="page_view">
-        <Board/>
+        {!inputDataCorrect &&
+            <div style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                height: "100vh"
+            }}>
+                <p style={{fontSize: "5vh", textAlign: "center"}}>READY TO PLAY A GAME? LET'S START!!!</p>
+                <UnderBoardBttn onClick={() => setOpenModal(true)} text="START NEW GAME"/>
+            </div>
+        }
+        <GameStartModal isOpen={openModal} onClose={() => setOpenModal(false)}/>
+        {inputDataCorrect && <Board/>}
     </div>
 }
