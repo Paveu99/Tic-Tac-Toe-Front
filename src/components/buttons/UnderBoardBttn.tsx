8d004e01024bfeda90ba84ed9025b363d6904b0a
Paweł Jarecki <paweljarecki10@gmail.com>
2024-03-21 20:46:53 +0100

    Button becomes more universal and not all the props must be passed.

--- src/components/buttons/UnderBoardBttn.tsx
+++ src/components/buttons/UnderBoardBttn.tsx
@@ -1,11 +1,12 @@
 import '../styles/UnderBoardBttn.scss'
 interface Props {
     text: string;
-    onClick: () => void;
+    onClick?: () => void;
+    type?: "submit" | "reset" | "button" | undefined;
 }
 
 export const UnderBoardBttn = (props: Props) => {
-    return <button className="underboard-button" onClick={props.onClick}>
+    return <button className="underboard-button" onClick={props.onClick} type={props.type}>
         {props.text}
     </button>
 }
