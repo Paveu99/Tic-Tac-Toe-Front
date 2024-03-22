711e5cd744e23b50d319e8ea76b9af006626d749
Paweł Jarecki <paweljarecki10@gmail.com>
2024-03-22 17:18:02 +0100

    Change in button behaviour: - button can be disabled by passing the props, - while :active it scales up to 1.1x of its normal size

--- src/components/buttons/UnderBoardBttn.tsx
+++ src/components/buttons/UnderBoardBttn.tsx
@@ -3,10 +3,12 @@ interface Props {
     text: string;
     onClick?: () => void;
     type?: "submit" | "reset" | "button" | undefined;
+    disabled?: boolean;
 }
 
 export const UnderBoardBttn = (props: Props) => {
-    return <button className="underboard-button" onClick={props.onClick} type={props.type}>
+
+    return <button className="underboard-button" onClick={props.onClick} type={props.type} disabled={props.disabled}>
         {props.text}
     </button>
 }
