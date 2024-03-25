2f5e6d1b88a9af2343371a966741a390db2e5121
Paweł Jarecki <paweljarecki10@gmail.com>
2024-03-25 21:36:23 +0100

    Time is being passed to the parent and also minutes are being counted

--- src/components/clock/Stopwatch.tsx
+++ src/components/clock/Stopwatch.tsx
@@ -4,9 +4,10 @@ import '../styles/Stopwatch.scss';
 interface Props {
     start: boolean;
     reset: boolean;
+    time: (time: string) => void;
 }
 
-export const Stopwatch = ({ start, reset }: Props) => {
+export const Stopwatch = ({ start, reset, time }: Props) => {
     const [isRunning, setIsRunning] = useState<boolean>(false);
     const [elapsedTime, setElapsedTime] = useState<number>(0);
     const intervalIdRef = useRef<NodeJS.Timeout | null>(null);
@@ -21,6 +22,7 @@ export const Stopwatch = ({ start, reset }: Props) => {
             }, 10) as NodeJS.Timeout;
         } else if (!start && isRunning) {
             setIsRunning(false);
+            time(formatTime());
             if (intervalIdRef.current) clearInterval(intervalIdRef.current);
         } else if (reset) {
             setIsRunning(false);
@@ -33,13 +35,16 @@ export const Stopwatch = ({ start, reset }: Props) => {
     }, [start, reset, isRunning, elapsedTime]);
 
     const formatTime = () => {
+
+        let minutes: number | string = Math.floor((elapsedTime / (1000 * 60)) % 60);
         let seconds: number | string = Math.floor((elapsedTime / 1000) % 60);
         let milliseconds: number | string = Math.floor((elapsedTime % 1000) / 10);
 
+        minutes = String(minutes).padStart(2, '0');
         seconds = String(seconds).padStart(2, '0');
         milliseconds = String(milliseconds).padStart(2, '0');
 
-        return `${seconds}:${milliseconds}`;
+        return `${minutes}:${seconds}:${milliseconds}`;
     };
 
     return (
