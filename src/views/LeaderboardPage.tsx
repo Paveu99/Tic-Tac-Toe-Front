b99264ee2a72482a9551c9da76fe5878b155d6b8
Paweł Jarecki <paweljarecki10@gmail.com>
2024-05-24 21:36:14 +0200

    Calling fetchRecords method from RecordProvider.tsx

--- src/views/LeaderboardPage.tsx
+++ src/views/LeaderboardPage.tsx
@@ -1,3 +1,18 @@
+import {useRecordContext} from "../components/context/RecordProvider.tsx";
+import {useEffect, useMemo} from "react";
+import {Table} from "../components/leaderboard/Table.tsx";
+
 export const LeaderboardPage = () => {
     return <h1>HEJ</h1>
+
+    const {allRecords, fetchRecords} = useRecordContext();
+
+    useEffect(() => {
+        fetchRecords();
+    }, []);
+
+    const memoizedData = useMemo(() => allRecords, [allRecords]);
+    
+    return <div>
+    </div>
 }
