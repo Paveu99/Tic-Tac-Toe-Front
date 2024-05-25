0d8b55b1377acd6b3fe0fa7db3c6efb7fda58d29
Paweł Jarecki <paweljarecki10@gmail.com>
2024-05-25 20:00:07 +0200

    Ability to paginate the results is implemented

--- src/components/leaderboard/Table.tsx
+++ src/components/leaderboard/Table.tsx
@@ -1,16 +1,45 @@
-import { MatchEntity } from "types"
-import {SingleElement} from "./SingleElement.tsx";
+import { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import { MatchEntity } from "types";
+import { SingleElement } from "./SingleElement";
+import "../styles/Pagination.scss";
+import "../styles/LeaderboardPage.scss"
 
 interface Props {
     records: MatchEntity[]
 }
 
-export const Table = ({records}: Props) => {
-    return <div>
-        {
-            records.map((el) => (
-                <SingleElement record={el} key={el.id}/>
-            ))
-        }
-    </div>
-}
+export const Table = ({ records }: Props) => {
+    const [currentPage, setCurrentPage] = useState(0);
+    const itemsPerPage = 15;
+
+    const handlePageClick = (data: { selected: number }) => {
+        setCurrentPage(data.selected);
+    };
+
+    const offset = currentPage * itemsPerPage;
+    const currentRecords = records.slice(offset, offset + itemsPerPage);
+    const pageCount = Math.ceil(records.length / itemsPerPage);
+
+    return (
+        <div className="table">
+            <div className="records">
+                {currentRecords.map((el,index) => (
+                    <SingleElement index={15*(currentPage)+(index+1)} record={el} key={el.id} />
+                ))}
+            </div>
+            <ReactPaginate
+                previousLabel={"\u2190"}
+                nextLabel={"\u2192"}
+                breakLabel={"..."}
+                breakClassName={"break-me"}
+                pageCount={pageCount}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={handlePageClick}
+                containerClassName={"pagination"}
+                activeClassName={"active"}
+            />
+        </div>
+    );
+};
